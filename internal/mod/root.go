f5d53f9b06312a66e58763d2786951c54147d0a4
m-quigley <m.quigley@live.com>
2024-11-05 23:01:31 +1100

    revert change

--- internal/mod/root.go
+++ internal/mod/root.go
@@ -2,27 +2,19 @@ package mod
 
 import (
 	"os/exec"
-	"path/filepath"
 	"strings"
 )
 
+// Root contains the absolute path of the module root. Its value is empty if
+// used outside of a module.
 var Root string
 
 func init() {
 	stdout, _ := exec.Command("go", "env", "GOMOD").Output()
 
-	// Trim spaces and check if the output ends with "/go.mod"
-	modPath := strings.TrimSpace(string(stdout))
 	var ok bool
-	Root, ok = strings.CutSuffix(modPath, "/go.mod")
+	Root, ok = strings.CutSuffix(strings.TrimSpace(string(stdout)), "/go.mod")
 	if !ok {
-		Root, ok = strings.CutSuffix(modPath, `\go.mod`) // Handle Windows path
-	}
-
-	if ok {
-		// Normalize the path separators for compatibility across platforms
-		Root = filepath.FromSlash(Root)
-	} else {
 		Root = ""
 	}
 }
